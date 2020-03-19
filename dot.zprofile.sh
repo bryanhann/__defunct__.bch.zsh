c3dd7fa924d1b6b325b9edb980be5f810167925e
Bryan Hann <nobody@nowhere>
2020-03-19 15:46:46 +0800

    Cosmetic change: 'venv' -> '3'

--- dot.zprofile.sh
+++ dot.zprofile.sh
@@ -1,26 +1,29 @@
-export PATH=$PATH:~/.local/bin
 source $(dirname $0)/__constants__
 
-install_venv () {
-    python3 -m venv ${BCH_SHELL_VENV}
-    source ${BCH_SHELL_VENV}/bin/activate
-    pip install --upgrade pip
-    pip install virtualenv
-    pip install virtualenvwrapper
-}
+export PATH=$PATH:~/.local/bin
 
-install_omzsh () {
-    URL=https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-    tSCRIPT=/tmp/$RANDOM.$RANDOM.sh
-    touch    ${tSCRIPT}
-    chmod +x ${tSCRIPT}
-    curl -fsSL ${URL} > ${tSCRIPT}
-    ${tSCRIPT} --unattended
-    rm ${tSCRIPT}
-    echo source ${BCH_SHELL_DIR}/dot.zshrc.sh > ${ZDOTDIR}/.zshrc``
-}
+#### INSTALL PYTHON 3 VIRTUAL ENVIRONMENT
+    install_venv () {
+        python3 -m venv ${BCH_SHELL_VENV}
+        source ${BCH_SHELL_VENV}/bin/activate
+        pip install --upgrade pip
+        pip install virtualenv
+        pip install virtualenvwrapper
+    }
+    __tryinstall__ VENV ${BCH_SHELL_VENV} install_venv
 
-__tryinstall__ VENV ${BCH_SHELL_VENV}_ install_venv
-__tryinstall__ Oh-My-ZSH ${ZSH} install_omzsh
+
+#### INASALL Oh-My-Zsh  ENVIRONMENT
+    install_omzsh () {
+        URL=https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+        tSCRIPT=/tmp/$RANDOM.$RANDOM.sh
+        touch    ${tSCRIPT}
+        chmod +x ${tSCRIPT}
+        curl -fsSL ${URL} > ${tSCRIPT}
+        ${tSCRIPT} --unattended
+        rm ${tSCRIPT}
+        echo source ${BCH_SHELL_DIR}/dot.zshrc.sh > ${ZDOTDIR}/.zshrc``
+    }
+    __tryinstall__ Oh-My-ZSH ${ZSH} install_omzsh
 
 
