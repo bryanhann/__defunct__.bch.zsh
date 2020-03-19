96e7cd5e036272beddf0c23f93fa54f34ab229ea
Bryan Hann <nobody@nowhere>
2020-03-19 10:09:23 +0800

    Move __BUILD__ to .__BUILD__

--- common.profile.sh
+++ common.profile.sh
@@ -1,9 +1,9 @@
 export BCH_SHELL_DIR=$(dirname $(grealpath $0))
-export BCH_SHELL_BLD=${BCH_SHELL_DIR}/__BUILD__
+export BCH_SHELL_BLD=${BCH_SHELL_DIR}/.__BUILD__
 
 export PATH=$PATH:~/.local/bin
 
 mkdir -p ${BCH_SHELL_DIR}
 
 export __BCHSHELL__=${BCH_SHELL_DIR}
-export BCH_SHELL_BUILD_DIR=${BCH_SHELL_DIR}/__BUILD__
+export BCH_SHELL_BUILD_DIR=${BCH_SHELL_BLD}
