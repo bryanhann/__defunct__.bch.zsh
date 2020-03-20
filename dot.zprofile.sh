7210738942f7dc0a5c6cc858d4155d5900ba1ff7
Bryan Hann <nobody@nowhere>
2020-03-20 19:59:47 +0800

    Touchup.

--- dot.zprofile.sh
+++ dot.zprofile.sh
@@ -3,7 +3,7 @@
 ##################################################################
     X="__root__(){echo $(dirname $(grealpath $0));}";eval "$X"
     source ~/.local/bch/dunders/dunders.sh
-    source $(dirname $0)/constants.sh
+    source $(__root__)/constants.sh
 
 #---------------------------------------------------------
 # Create vital directories
