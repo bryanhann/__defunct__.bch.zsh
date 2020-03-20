49f2e4523f49cbeed52d2aa26371bf7414e32164
Bryan Hann <nobody@nowhere>
2020-03-20 11:49:00 +0800

    Merge pip management into __constants__

--- misc.sh
+++ misc.sh
@@ -3,5 +3,4 @@ source $(dirname $0)/misc.d/colors
 source $(dirname $0)/misc.d/rxx
 source $(dirname $0)/misc.d/watch
 source $(dirname $0)/misc.d/XDG
-source $(dirname $0)/misc.d/pip
 source $(dirname $0)/misc.d/misc
