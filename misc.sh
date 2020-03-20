2b5e9b262b2b991db4b1c660539380507fa2c5a5
Bryan Hann <nobody@nowhere>
2020-03-20 12:00:14 +0800

    Append '.sh' to various scripts

--- misc.sh
+++ misc.sh
@@ -1,6 +1,5 @@
-source $(dirname $0)/misc.d/nesting
-source $(dirname $0)/misc.d/colors
-source $(dirname $0)/misc.d/rxx
-source $(dirname $0)/misc.d/watch
-source $(dirname $0)/misc.d/XDG
-source $(dirname $0)/misc.d/misc
+source $(dirname $0)/misc.d/nestin.sh
+source $(dirname $0)/misc.d/colors.sg
+source $(dirname $0)/misc.d/rxx.sg
+source $(dirname $0)/misc.d/watch.sg
+source $(dirname $0)/misc.d/misc.sh
