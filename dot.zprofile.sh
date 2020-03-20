fecb869d5169336cc342162579028cc4ee0aa827
Bryan Hann <nobody@nowhere>
2020-03-20 14:37:32 +0800

    Factor out installation of venv and omzsh.
    Also move the writing of .zshrc to DOTDIR out of the
    installer and into profile.

--- dot.zprofile.sh
+++ dot.zprofile.sh
@@ -5,30 +5,9 @@ export PATH=$PATH:~/.local/bin
 
 [ -d ${BCH_SHELL_BLD} ] || mkdir ${BCH_SHELL_BLD}
 [ -d ${ZDOTDIR}       ] || mkdir ${ZDOTDIR}
+echo source ${BCH_SHELL_DIR}/dot.zshrc.sh > ${ZDOTDIR}/.zshrc``
+
+source $(dirname $0)/install.venv.sh
+source $(dirname $0)/install.omzsh.sh
+
 __link__ pip
-
-#### INSTALL PYTHON 3 VIRTUAL ENVIRONMENT
-    install_venv () {
-        python3 -m venv ${BCH_SHELL_VENV}
-        source ${BCH_SHELL_VENV}/bin/activate
-        pip install --upgrade pip
-        pip install virtualenv
-        pip install virtualenvwrapper
-    }
-    __tryinstall__ VENV ${BCH_SHELL_VENV} install_venv
-
-
-#### INASALL Oh-My-Zsh  ENVIRONMENT
-    install_omzsh () {
-        URL=https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-        tSCRIPT=/tmp/$RANDOM.$RANDOM.sh
-        touch    ${tSCRIPT}
-        chmod +x ${tSCRIPT}
-        curl -fsSL ${URL} > ${tSCRIPT}
-        ${tSCRIPT} --unattended
-        rm ${tSCRIPT}
-        echo source ${BCH_SHELL_DIR}/dot.zshrc.sh > ${ZDOTDIR}/.zshrc``
-    }
-    __tryinstall__ Oh-My-ZSH ${ZSH} install_omzsh
-
-
