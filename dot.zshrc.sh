3432362e295a4d2c89e7e50648436295ed494135
Bryan Hann <nobody@nowhere>
2020-03-21 21:41:39 +0800

    Remove call to constants from dot.bashrc.sh

--- dot.zshrc.sh
+++ dot.zshrc.sh
@@ -1,12 +1,9 @@
-##################################################################
-#   dot.zshrc
-##################################################################
-
-X="__root__(){echo $(dirname $(grealpath $0));}";eval "$X"
-source ~/.config/bch/constants.sh
-source $(__root__)/constants.sh
+echo ++ dot.zshrc.sh
+XX="__root__(){echo $(dirname $(grealpath $0));}"
+eval "${XX}"
 source $(__root__)/zshrc.omzsh.sh
 source $(__root__)/misc.colors.sh
 source $(__root__)/misc.rxx.sh
 source $(__root__)/misc.misc.sh
+echo -- dot.zshrc.sh
 
